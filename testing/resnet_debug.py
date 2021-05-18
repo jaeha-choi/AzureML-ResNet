f4a8353679a647a12b1d2e225272ed955242aca5
Jaeha Choi <jaeha220@gmail.com>
2021-05-17 18:22:05 -0700

    Update to use logger

--- testing/resnet_debug.py
+++ testing/resnet_debug.py
@@ -1,3 +1,5 @@
+import logging as log
+
 import torch.nn as nn
 import torch.optim as opt
 from torch.utils.data import DataLoader
@@ -5,8 +7,11 @@ from torch.utils.data import DataLoader
 from src.resnet50_15 import Resnet50v15, Resnet50v15Classifier
 from util.tiny_imagenet import TinyImagenet
 
+log.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
+                level=log.INFO, datefmt='%m/%d/%Y %I:%M:%S %p')
+
 # ------- Model parameters ------- #
-EPOCH = 5
+EPOCH = 1
 LEARNING_RATE = 0.1
 BATCH_SIZE = 3
 SHUFFLE_DATA = True
@@ -24,18 +29,18 @@ optimizer = opt.SGD(resnet_classifier.parameters(), lr=LEARNING_RATE, momentum=0
 scheduler = opt.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=10)
 
 for epoch in range(EPOCH):
-    print("\nEpoch: %s" % epoch)
+    log.info("Epoch: %s" % epoch)
     running_loss = 0
     for i, (img, label) in enumerate(train_dataloader):
         # Train models here
-        print("1")
+        log.debug("1")
         optimizer.zero_grad()
         loss = loss_fn(resnet_classifier(img), label)
         loss.backward()
-        print("2")
+        log.debug("2")
         scheduler.step(loss)
         running_loss += loss.item()
         if (i + 1) % 10 == 0:
-            print("Epoch: %s\tBatch: %s\tLoss: %s" % (epoch + 1, i + 1, running_loss))
+            log.info("Epoch: %s\tBatch: %s\tLoss: %s" % (epoch + 1, i + 1, running_loss))
         # break
-    break
+    # break
