9b82b51857efc8b6384f52197610b99d1837b2d9
Jaeha Choi <jaeha220@gmail.com>
2021-05-18 23:02:21 -0700

    Add placeholder for validation dataset class

---
+++ util/tiny_imagenet_val.py
@@ -0,0 +1,13 @@
+import torch.utils.data
+
+
+class TinyImagenet(torch.utils.data.Dataset):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
